d65c419672bed13bdbb610712aecfe4b581a2993
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-08-28 21:43:24 +0300

    Создан прототип

---
+++ src/test/kotlin/me/func/poker/PokerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.func.poker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PokerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
